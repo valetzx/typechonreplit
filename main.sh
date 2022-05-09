7e500126cc752e13f7bf446e6da80b277702b621
valetzx <45054400+valetzx@users.noreply.github.com>
2022-05-09 23:04:06 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,10 @@ rm -rf database.sqlite
 rm -rf README.md
 wget https://github.com/typecho/typecho/releases/download/v1.2.0/typecho.zip
 unzip typecho.zip
+rm -rf typecho.zip
 cd usr/themes
 wget https://github.com/wehaox/Typecho-Butterfly/releases/download/1.5.0/butterfly_Release1.5.0.zip
 unzip butterfly_Release1.5.0.zip
-#after download if you dont have reCAPTCHA plugin del line 
+#after download if you dont have reCAPTCHA plugin del file usr/themes/butterfly/functions.php line 1528-1529 
+rm -rf butterfly_Release1.5.0.zip
+#when all down run run bottom
