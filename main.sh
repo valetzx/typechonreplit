bbb24595a3e253d4ed938e35bf7b994d87a0ae07
valetzx <45054400+valetzx@users.noreply.github.com>
2022-05-09 22:57:49 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,8 @@
+rm -rf index.php
+rm -rf database.sqlite
+rm -rf README.md
+wget https://github.com/typecho/typecho/releases/download/v1.2.0/typecho.zip
+unzip typecho.zip
+cd usr/themes
+wget https://github.com/wehaox/Typecho-Butterfly/releases/download/1.5.0/butterfly_Release1.5.0.zip
+unzip butterfly_Release1.5.0.zip
